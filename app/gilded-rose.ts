8959262beb7a41ce120cdbba9dc1cbed5b620445
AZKRON\Hugo <hugobeny@gmail.com>
2018-10-17 13:29:07 +0200

    Created item tracker classes

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,6 +10,93 @@ export class Item {
     }
 }
 
+
+function isConjured(item: Item){
+    return item.name.substring(0,8) == "Conjured";
+}
+
+interface ItemTracker{
+    updateQuality();
+}
+
+class LegendaryTracker implements ItemTracker{
+    updateQuality(){};
+}
+
+class NormalTracker implements ItemTracker{
+    private qualityMod: number = 1;
+    constructor(private item: Item)
+    {
+        if(isConjured(item))
+            this.qualityMod = 2;
+        
+        if(this.item.sellIn == 0)
+            this.qualityMod *= 2;
+        
+    }
+
+    updateQuality(){
+        this.item.sellIn -= 1;
+        if(this.item.sellIn == 0)
+            this.qualityMod *= 2;
+
+        if(this.item.quality > 0){
+            this.item.quality = this.item.quality >= this.qualityMod ? 
+                this.item.quality - this.qualityMod : 0;
+        }
+    }
+}
+
+class AgingTracker implements ItemTracker{
+    constructor(private item: Item){}
+
+    updateQuality(){
+        this.item.sellIn -= 1;
+
+        if(this.item.quality < 50){
+            this.item.quality += 1;
+        }
+    }
+}
+
+class EventTracker implements ItemTracker{
+    private qualityMod: number = 1;
+    constructor(private item: Item)
+    {
+        if(this.item.sellIn <= 0)
+            this.item.quality = 0;
+        else if(this.item.sellIn <= 5)
+            this.qualityMod = 3;
+        else if(this.item.sellIn <= 10)
+            this.qualityMod = 2;
+        
+    }
+
+    updateQuality(){
+        if(this.item.sellIn <= 0){
+            this.item.sellIn -= 1;
+            return;
+        }
+        else{
+            this.item.sellIn -= 1;
+
+            if(this.item.sellIn == 10)
+                this.qualityMod = 2;
+            else if (this.item.sellIn == 5)
+                this.qualityMod = 3;
+            else if(this.item.sellIn == 0){
+                this.item.quality = 0;
+                return;
+            }
+
+            if(this.item.quality > 0){
+                this.item.quality = 50 - this.item.quality >= this.qualityMod ? 
+                    this.item.quality += this.qualityMod : 50;
+            }
+        }
+    }
+}
+
 export class GildedRose {
     items: Array<Item>;
 
@@ -19,6 +106,8 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
+
+            // OLD CODE
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -42,9 +131,11 @@ export class GildedRose {
                     }
                 }
             }
+
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
+
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
